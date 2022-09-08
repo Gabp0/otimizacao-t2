df07b76640239ca7b096c3f648db3686d14a4656
gab <gabriel.pontarolo045@gmail.com>
2022-09-07 22:22:12 -0300

    argumentos na linha de comando

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
 #include "cast.h"
+#include <algorithm>
 
-int main(int argc, char const *argv[])
+using namespace std;
+
+bool argExists(int argc, char **argv, const string &option)
+{
+    return find(argv, argc + argv, option) != (argv + argc);
+}
+
+int main(int argc, char *argv[])
 {
     Cast c;
 
-    c.toggleDefaultFunc();
-    // c.toggleOptimalityCut();
-    //  c.toggleViabilityCut();
+    if (argExists(argc, argv, "-f"))
+        c.toggleViabilityCut();
+
+    if (argExists(argc, argv, "-o"))
+        c.toggleOptimalityCut();
+
+    if (argExists(argc, argv, "-a"))
+        c.toggleDefaultFunc();
 
     c.branchAndBound();
-
     c.showResults();
 
     return 0;
