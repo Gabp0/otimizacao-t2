ddfd8df1c50f281baf08ff0421efdceabf5f3690
gab <gabriel.pontarolo045@gmail.com>
2022-09-08 19:07:47 -0300

    gera input

---
+++ geraInput.py
@@ -0,0 +1,18 @@
+from random import randint
+
+l, m, n = input().split(" ")
+
+for i in range(0, int(m)):
+    gNum = randint(1, int(l))
+
+    value = randint(9, gNum*10)
+    groups = set()
+
+    while len(groups) != gNum:
+        groups.add(randint(1, int(l)))
+    
+    print(f"{value} {gNum}")
+    for group in groups:
+        print(group)
+    
+        
